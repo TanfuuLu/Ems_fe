d35cb2486b9acac80cd2c3fd60885bc4b594bc3f
Tanfuu Lu <lutanfuu@gmail.com>
2025-02-08 19:31:31 +0700

    create frontend angular for ems

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import { Config } from 'tailwindcss';
+
+const config: Config = {
+    content: [
+        './src/**/*.{html,ts,tsx,js,jsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+export default config;
